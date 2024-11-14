4a6e6b01110303220be6a5b524acd57eccf034cc
BranGitfox <vixfgit@gmail.com>
2024-11-14 23:08:33 +0300

    feat: adding joke container

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <div className="w-[100%] md:w-[1100px]  m-auto">
+    <div className="w-[100%] md:w-[1100px]  m-auto bg-green-200">
         <nav className="w-[100%] h-[70px] bg-green-500 flex justify-between items-center p-3">
             <h2 className="text-white text-2xl font-bold">J🤪keLand </h2>
             <select className="p-3 bg-green-800 text-white rounded-md" name="" id="">
@@ -15,6 +15,45 @@ function App() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolorum excepturi asperiores nisi quasi itaque libero nostrum delectus optio distinctio, tempora sapiente omnis nobis odio doloremque soluta ab voluptatum corporis.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui nam perferendis unde incidunt placeat obcaecati asperiores. Veritatis sed rem libero nihil consequatur ducimus dignissimos? Earum repudiandae aperiam dicta mollitia!
         </section>
+        <hr className="border-green-300"/>
+        <div className="mt-5 flex flex-wrap w-[100%] gap-4 p-3 justify-evenly">
+              <div className="w-[300px] relative bg-red-300 ">
+                  <div className="w-[100%] p-3 mb-8">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+                  <button className=" py-3 px-5 bg-green-950 text-white  bottom-0 left-0 right-0 absolute">Show Answer</button>
+                  <div className="w-[100%] p-3 mb-10">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+              </div>
+              <div className="w-[300px] relative bg-red-300 ">
+                  <div className="w-[100%] p-3 mb-8">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+                  <button className=" py-3 px-5 bg-green-950 text-white  bottom-0 left-0 right-0 absolute">Show Answer</button>
+                  <div className="w-[100%] p-3 mb-10">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+              </div>
+              <div className="w-[300px] relative bg-red-300 ">
+                  <div className="w-[100%] p-3 mb-8">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+                  <button className=" py-3 px-5 bg-green-950 text-white  bottom-0 left-0 right-0 absolute">Show Answer</button>
+                  <div className="w-[100%] p-3 mb-10">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+              </div>
+              <div className="w-[300px] relative bg-red-300 ">
+                  <div className="w-[100%] p-3 mb-8">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+                  <button className=" py-3 px-5 bg-green-950 text-white  bottom-0 left-0 right-0 absolute">Show Answer</button>
+                  <div className="w-[100%] p-3 mb-10">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis tempora quasi quas magnam cum repellat excepturi ducimus temporibus hic, quae velit, blanditiis reprehenderit maiores aliquid amet expedita debitis qui libero.
+                  </div>
+              </div>
+        </div>
     </div>
   )
 }
