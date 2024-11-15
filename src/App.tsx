d377fa04cfc7fe2bba6240cbe9c01230dd71e106
BranGitfox <vixfgit@gmail.com>
2024-11-15 17:21:04 +0300

    feat: adding switch text lang text

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,12 @@ import { Jok } from "./components/JokCard"
 function App() {
   const [lang, setLang] = useState<string>('en')
   const changeLang = (lang:string) => {
-    setLang(lang)
-    console.log(lang);
-    
+    setLang(lang)  
   }
 
   
   const {data, isLoading} = useGetJoksQuery(lang)
+
 
 
  
@@ -22,7 +21,7 @@ function App() {
  
  
   return (
-    <div className="w-[100%] md:w-[1100px] min-h-[100vh]  m-auto bg-green-200">
+    <div className="w-[100%] md:w-[1100px] min-h-[100vh]  m-auto bg-green-200 ">
         <nav className="w-[100%] h-[70px] bg-green-500 flex justify-between items-center p-3">
 
             <h2 className="text-white text-2xl font-bold">J🤪keLand </h2>
@@ -33,7 +32,7 @@ function App() {
         </nav>
         <section className="mt-10 w-[100%] p-3">
             <h1 className="text-4xl mb-3 text-center font-bold ">Haahahahahahah 🤣</h1>
-            <p className="text-center font-medium">Bad day, owwwwh , don't worry 😇, this website gonna made your day!!!</p>
+            <p className="text-center font-medium">{lang==='fr'?'Mauvaise journee?, owwwwh, ne vous inquietez pas, ce site va vous faire votre journee':'Bad day, owwwwh , don\'t worry 😇, this website gonna made your day!!!'}</p>
         </section>
         <hr className="border-green-300"/>
         <div className="mt-5 flex flex-wrap w-[100%] gap-4 p-3 justify-evenly">
@@ -48,7 +47,9 @@ function App() {
         data.jokes.map((jok:Jok, index:number) => <JokCard key={index} lang={jok.lang} setup={jok.setup} delivery={jok.delivery}/>)
        }
         </div>
-        
+        <footer className="w-[100%]">
+          <p className="text-center relative md:top-10">Made with ❤️, by <a className="font-bold underline" href="https://github.com/BranGitfox">BranGitfox</a> 2024</p>
+        </footer>
     </div>
   )
 }
