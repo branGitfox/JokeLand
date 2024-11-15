c425a112bbc17008fed29aed70c5487391d03fca
BranGitfox <vixfgit@gmail.com>
2024-11-15 17:09:10 +0300

    feat: adding lang selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import JokCard from "./components/JokCard"
 import { useGetJoksQuery } from "./assets/app/service/JokAPI"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { PacmanLoader } from "react-spinners"
 import { Jok } from "./components/JokCard"
 
 function App() {
-  const [lang, setLang] = useState<string>('fr')
+  const [lang, setLang] = useState<string>('en')
   const changeLang = (lang:string) => {
     setLang(lang)
     console.log(lang);
     
   }
 
-  const [joks, setJoks] = useState<Jok[]>([])
-  
-
   
   const {data, isLoading} = useGetJoksQuery(lang)
 
+
  
-  useEffect(() => {
-  }, [lang])
-  
+
+ 
+ 
  
   return (
     <div className="w-[100%] md:w-[1100px] min-h-[100vh]  m-auto bg-green-200">
@@ -29,16 +27,17 @@ function App() {
 
             <h2 className="text-white text-2xl font-bold">J🤪keLand </h2>
             <select onChange={(e) => changeLang(e.target.value)}  className="p-3 bg-green-800 text-white rounded-md" >
-                <option value="eng">ENG</option>
+                <option value="en">ENG</option>
                 <option  value="fr">FR</option>
             </select>
         </nav>
         <section className="mt-10 w-[100%] p-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolorum excepturi asperiores nisi quasi itaque libero nostrum delectus optio distinctio, tempora sapiente omnis nobis odio doloremque soluta ab voluptatum corporis.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui nam perferendis unde incidunt placeat obcaecati asperiores. Veritatis sed rem libero nihil consequatur ducimus dignissimos? Earum repudiandae aperiam dicta mollitia!
+            <h1 className="text-4xl mb-3 text-center font-bold ">Haahahahahahah 🤣</h1>
+            <p className="text-center font-medium">Bad day, owwwwh , don't worry 😇, this website gonna made your day!!!</p>
         </section>
         <hr className="border-green-300"/>
         <div className="mt-5 flex flex-wrap w-[100%] gap-4 p-3 justify-evenly">
+          
         {isLoading?<PacmanLoader  color={'green'}
         loading={isLoading}
         
